e687febbfb0eb701216291fa345fcc7a4c863ccc
HwangInHo1217 <hih1217@skuniv.ac.kr>
2023-12-21 17:21:03 +0900

    ë거스름돈 추가

---
+++ greedy/src/CompetitionOrIntern.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class CompetitionOrIntern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, m, k;
+        n=sc.nextInt();
+        m=sc.nextInt();
+        k=sc.nextInt();
+        if (n*2==)
+    }
+}
